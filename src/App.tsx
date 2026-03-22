80bb39a462d41e0060cdb607eb340a52d8a37da2
rahul-kapgate <rahulkapgateyt@gmail.com>
2026-03-22 11:03:45 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <h1 className="text-3xl font-bold underline bg-red-600">
     Hello world!
   </h1>
+  <Button>test shacd cn</Button>
  </>
   )
 }
